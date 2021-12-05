72c16e4a2ac062761fed49025c9c2814b56648bc
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-12-05 13:01:24 +1000

    Working example with dynamodb

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/mvanbrummen/website-uptime-probe
 
 go 1.16
+
+require (
+	github.com/aws/aws-sdk-go v1.42.19 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/guregu/dynamo v1.11.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
+	golang.org/x/net v0.0.0-20211203184738-4852103109b8 // indirect
+	golang.org/x/sys v0.0.0-20211204120058-94396e421777 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+)
+
+type widget struct {
+	UserID int       `dynamo:",hash"`  // Hash key, a.k.a. partition key
+	Time   time.Time `dynamo:",range"` // Range key, a.k.a. sort key
+
+	Msg string `dynamo:"Message"` // Change name in the database
+}
+
 func main() {
+	awsRegion := os.Getenv("AWS_REGION")
+	dynamdoEndpoint := os.Getenv("AWS_DYNAMO_ENDPOINT")
+
+	sess := session.Must(session.NewSession())
+	db := dynamo.New(sess, &aws.Config{
+		Region:   aws.String(awsRegion),
+		Endpoint: aws.String(dynamdoEndpoint),
+	})
+
+	db.CreateTable("Widgets", &widget{}).Run()
+
+	table := db.Table("Widgets")
+
+	// put item
+	w := widget{UserID: 613, Time: time.Now(), Msg: "hello"}
+	err := table.Put(w).Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// get the same item
+	var result widget
+	err = table.Get("UserID", w.UserID).
+		Range("Time", dynamo.Equal, w.Time).
+		One(&result)
+
+	log.Println(result)
 
 }
